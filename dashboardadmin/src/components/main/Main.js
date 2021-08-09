e32e7b8d932fa37a7ed6508a8ed369a76a91a94b
Mateus Campos <mateuscamposantos@gmail.com>
2021-08-09 17:22:54 -0300

    Corrigindo fechamento da div

--- dashboardadmin/src/components/main/Main.js
+++ dashboardadmin/src/components/main/Main.js
@@ -44,48 +44,48 @@ const Main = () => {
               <span className="font-bold text-title">40</span>
             </div>
           </div>
+        </div>
 
-          <div className="charts">
-            <div className="charts__left">
-              <div className="charts__left__title">
-                <div>
-                  <h1>Daily Reports</h1>
-                  <p>Ubatuba, São paulo, BR</p>
-                </div>
-                <i className="fa fa-usd"></i>
+        <div className="charts">
+          <div className="charts__left">
+            <div className="charts__left__title">
+              <div>
+                <h1>Daily Reports</h1>
+                <p>Ubatuba, São paulo, BR</p>
               </div>
-              <Chart />
+              <i className="fa fa-usd"></i>
+            </div>
+            <Chart />
+          </div>
+
+          <div className="right">
+            <div className="charts__right__title">
+              <div>
+                <h1>Daily Reports</h1>
+                <p>Ubatuba, São paulo, BR</p>
+              </div>
+              <i className="fa fa-area-chart"></i>
             </div>
 
-            <div className="right">
-              <div className="charts__right__title">
-                <div>
-                  <h1>Daily Reports</h1>
-                  <p>Ubatuba, São paulo, BR</p>
-                </div>
-                <i className="fa fa-area-chart"></i>
+            <div className="charts__right__cards">
+              <div className="card1">
+                <h1>Lucro</h1>
+                <p>R$2.500,00</p>
               </div>
 
-              <div className="charts__right__cards">
-                <div className="card1">
-                  <h1>Lucro</h1>
-                  <p>R$2.500,00</p>
-                </div>
+              <div className="card2">
+                <h1>Pagamentos</h1>
+                <p>R$250,00</p>
+              </div>
 
-                <div className="card2">
-                  <h1>Pagamentos</h1>
-                  <p>R$250,00</p>
-                </div>
+              <div className="card3">
+                <h1>Custos de hospedagem</h1>
+                <p>R$150,00</p>
+              </div>
 
-                <div className="card3">
-                  <h1>Custos de hospedagem</h1>
-                  <p>R$150,00</p>
-                </div>
-
-                <div className="card4">
-                  <h1>Banco de dados</h1>
-                  <p>R$190,00</p>
-                </div>
+              <div className="card4">
+                <h1>Banco de dados</h1>
+                <p>R$190,00</p>
               </div>
             </div>
           </div>
