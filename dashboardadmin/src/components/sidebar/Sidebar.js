cb82b292341cb752a165073e5736e36217875e83
Mateus Campos <mateuscamposantos@gmail.com>
2021-08-11 11:26:28 -0300

    Update Sidebar.js

--- dashboardadmin/src/components/sidebar/Sidebar.js
+++ dashboardadmin/src/components/sidebar/Sidebar.js
@@ -19,53 +19,41 @@ const Sidebar = ({ sidebarOpen, closeSidebar }) => {
       </div>
       <div className="sidebar__menu">
         <div className="sidebar__link active_menu_link">
-          <i className="fa fa-minus-square"></i>
-          <a href="#">Home</a>
+          <a href="/">Home</a>
         </div>
         <h2>ADMIN</h2>
         <div className="sidebar__link">
-          <i className="fa fa-tachometer"></i>
           <a href="#">Área administrativa</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-building"></i>
           <a href="#">Lojas</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-archive"></i>
           <a href="#">Produtos</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-bars"></i>
           <a href="#">Categorias</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-cutlery"></i>
           <a href="#">Pedidos</a>
         </div>
         <h2>PESSOAS</h2>
         <div className="sidebar__link">
-          <i className="fa fa-male"></i>
           <a href="#">Administradores</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-user-circle"></i>
           <a href="#">Usuarios</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-money"></i>
           <a href="#">Pagamentos e custos</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-tasks"></i>
           <a href="#">A plataforma</a>
         </div>
         <div className="sidebar__link">
-          <i className="fa fa-file-text"></i>
           <a href="#">Politica de privacidade</a>
         </div>
         <div className="sidebar__logout">
-          <i className="fa fa-power-off"></i>
           <a href="#">Log out</a>
         </div>
       </div>
