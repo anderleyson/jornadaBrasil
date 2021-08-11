ee62d7d542b8d360bf575cda93cc4384291a0c56
anderleyson <anderleyy15@gmail.com>
2021-08-11 18:59:13 -0300

    Commit]

--- src/config/connection.js
+++ src/config/connection.js
@@ -4,7 +4,8 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'jornada_brasil'
+  database: process.env.DB_NAME || 'jornada_brasil',
+  port: '3306'
 });
 connection.connect((err)=> {
   if(err){
