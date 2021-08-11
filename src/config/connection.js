8fad1ea5f9a88e585fa45e15b05a9e7f43162912
anderleyson <anderleyy15@gmail.com>
2021-08-11 20:37:15 -0300

    Atualização

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,18 +1,33 @@
 const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
-
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME 
-});
-connection.connect((err)=> {
-  if(err){
-      console.log("Erro ao conectar com o banco!\n" + err)
-  }else{
-      console.log("Conectado com o banco!")
+for (;;) {
+    try {
+        const connection =  mysql.createConnection({
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME 
+        });
+        connection.connect((err)=> {
+            if(err){
+                console.log("Erro ao conectar com o banco!\n" + err)
+            }else{
+                console.log("Conectado com o banco!")
+            }
+            
+        });
+      try {
+        // do stuff with conn
+      } catch (err) {
+        console.log(`Error doing stuff: ${err.message}`);
+      } finally {
+        connection.destroy();
+      }
+    } catch (err) {
+      console.log(`Unable to acquire connection: ${err.message}`);
+    }
+    // delay before trying to reacquire connection
   }
-  
-});
+
+
 module.exports = connection;
