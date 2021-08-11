9071259f849d8a14e9b36c47ef36e1029b505a15
anderleyson <anderleyy15@gmail.com>
2021-08-11 04:04:34 -0300

    Update

--- conn.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    
-        HOST: "us-cdbr-east-04.cleardb.com",
-        USER: "bf79a0775136e5",
-        PASSWORD: "4dce57b6",
-        DB: "heroku_c46aa5ba021394d"
-      
-}
--- src/config/connection.js
+++ src/config/connection.js
@@ -1,8 +1,13 @@
 const mysql = require('mysql2')
-const config = require('../../config');
 
 
-const connection = mysql.createConnection(config.production);
+
+const connection = mysql.createConnection({
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD    
+});
 connection.connect((err)=> {
     if(err){
         console.log("Erro ao conectar com o banco!\n" + err)
