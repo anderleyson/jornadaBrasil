d9db651d8c6f6ed75f72156154cdba5c437fd1ed
anderleyson <anderleyy15@gmail.com>
2021-08-11 19:04:08 -0300

    Commit]

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
