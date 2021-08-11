1508c4fa5757b57ae6964557e9c72f8416ca9f7f
anderleyson <anderleyy15@gmail.com>
2021-08-11 18:55:24 -0300

    Commit]

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME 
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'jornada_brasil'
 });
 connection.connect((err)=> {
   if(err){
