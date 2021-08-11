0dee290b7b18bdbc546e4366a30c3ca0b9612ce0
anderleyson <anderleyy15@gmail.com>
2021-08-11 03:29:32 -0300

    Update

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,13 +1,8 @@
 const mysql = require('mysql2')
-require('dotenv').config();
+const config = require('../../config');
 
 
-const connection = mysql.createConnection({
-      host: process.env.DATABASE_HOST,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD    
-});
+const connection = mysql.createConnection(config.production);
 connection.connect((err)=> {
     if(err){
         console.log("Erro ao conectar com o banco!\n" + err)
