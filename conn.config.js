8918ff49865a6af93a4ecc927774072499ba205b
anderleyson <anderleyy15@gmail.com>
2021-08-11 03:11:20 -0300

    Update

---
+++ conn.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    
+        HOST: "us-cdbr-east-04.cleardb.com",
+        USER: "bf79a0775136e5",
+        PASSWORD: "4dce57b6",
+        DB: "heroku_c46aa5ba021394d"
+      
+}
--- src/config/connection.js
+++ src/config/connection.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 
 const connection = mysql.createConnection({
-      host: process.env.DATABASE_HOST || 'localhost',
-      database: process.env.DATABASE_NAME || 'jornada_brasil',
-      user: process.env.DATABASE_USERNAME || 'root',
-      password: process.env.DATABASE_PASSWORD || 'root'    
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD    
 });
 connection.connect((err)=> {
     if(err){
