1f04438ad27682b6cb15849cbb0733e99e92c19a
anderleyson <anderleyy15@gmail.com>
2021-08-11 20:49:00 -0300

    Percebendo erro

--- src/config/connection.js
+++ src/config/connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2')
 for (;;) {
     try {
         const connection =  mysql.createConnection({
-            host: process.env.DATABASE_HOST,
-            user: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
-            database: process.env.DATABASE_NAME 
+          host: process.env.DB_HOST,
+          user: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME 
         });
         connection.connect((err)=> {
             if(err){
@@ -27,7 +27,25 @@ for (;;) {
       console.log(`Unable to acquire connection: ${err.message}`);
     }
     // delay before trying to reacquire connection
+
+/*const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME 
+
+
+  host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME
+});
+connection.connect((err)=> {
+  if(err){
+      console.log("Erro ao conectar com o banco!\n" + err)
+  }else{
+      console.log("Conectado com o banco!")
   }
 
-
+*/
 module.exports = connection;
