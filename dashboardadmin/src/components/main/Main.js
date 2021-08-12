663f98e9d64348986a3152141a51d7bea853de22
Mateus Campos <mateuscamposantos@gmail.com>
2021-08-12 20:38:43 -0300

    Update Main.js

--- dashboardadmin/src/components/main/Main.js
+++ dashboardadmin/src/components/main/Main.js
@@ -14,22 +14,22 @@ const Main = () => {
         <div className="main__cards">
           <div className="card">
             <div className="card_inner">
-              <p className="text-primary-p">Número de pedidos</p>
-              <span className="font-bold text-title">578</span>
+              <p className="text-primary-p">Números de roteiros</p>
+              <span className="font-bold text-title">64</span>
             </div>
           </div>
 
           <div className="card">
             <div className="card_inner">
-              <p className="text-primary-p">Pagamentos</p>
-              <span className="font-bold text-title">R$2.467,00</span>
+              <p className="text-primary-p">Cidades cadastradas</p>
+              <span className="font-bold text-title">45</span>
             </div>
           </div>
 
           <div className="card">
             <div className="card_inner">
-              <p className="text-primary-p">Número de produtos</p>
-              <span className="font-bold text-title">670</span>
+              <p className="text-primary-p">Galeria</p>
+              <span className="font-bold text-title">69</span>
             </div>
           </div>
 
@@ -37,40 +37,6 @@ const Main = () => {
             <div className="card_inner">
               <p className="text-primary-p">Categorias</p>
               <span className="font-bold text-title">40</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="charts">
-          <div className="right">
-            <div className="charts__right__title">
-              <div>
-                <h1>Daily Reports</h1>
-                <p>Ubatuba, São paulo, BR</p>
-              </div>
-              <i className="fa fa-area-chart"></i>
-            </div>
-
-            <div className="charts__right__cards">
-              <div className="card1">
-                <h1>Lucro</h1>
-                <p>R$2.500,00</p>
-              </div>
-
-              <div className="card2">
-                <h1>Pagamentos</h1>
-                <p>R$250,00</p>
-              </div>
-
-              <div className="card3">
-                <h1>Custos de hospedagem</h1>
-                <p>R$150,00</p>
-              </div>
-
-              <div className="card4">
-                <h1>Banco de dados</h1>
-                <p>R$190,00</p>
-              </div>
             </div>
           </div>
         </div>
