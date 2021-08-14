c2db168d6f8acce9c056e036343929c225bf0601
anderleyson <anderleyy15@gmail.com>
2021-08-14 02:07:03 -0300

    Commits finais

--- src/app/controllers/regiaoController.js
+++ src/app/controllers/regiaoController.js
@@ -30,7 +30,7 @@ class RegiaoController{
         getRegiao(req, res){
             const connection = connectionRequest();
             let id = req.params.id;
-            connection.query('SELECT r.id_regiao, r.desc_regiao, r.culinaria, r.foto_capa_regiao, r.foto_prato, r.foto_principal_regiao, e.id_estado, e.foto_principal_estado, ro.id_roteiro, ro.foto_principal_roteiro FROM regioes r INNER JOIN estados e ON r.id_regiao = e.id_regiao INNER JOIN roteiros ro ON e.id_estado = ro.id_estado WHERE r.id_regiao = "'+ id +'" ORDER BY rand() LIMIT 3;', function(err, rows, fields){
+            connection.query('SELECT r.id_regiao, r.conheca, r.desc_regiao, r.culinaria, r.cultura, r.foto_capa_regiao, r.foto_prato, r.foto_principal_regiao, e.id_estado, e.foto_principal_estado, ro.id_roteiro, ro.foto_principal_roteiro FROM regioes r INNER JOIN estados e ON r.id_regiao = e.id_regiao INNER JOIN roteiros ro ON e.id_estado = ro.id_estado WHERE r.id_regiao = "'+ id +'" ORDER BY rand() LIMIT 3;', function(err, rows, fields){
                 if (err) throw err
     
                 res.send(rows);
